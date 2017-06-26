436c1b4300d374dabf6924f5d2dc512a986e1f61
Ziad Zorgati <Ziad.Zorgati@gmx.de>
2017-06-26 05:37:28 +0200

    spec refactoring  - use subject and one liner for better and clear specs

--- spec/downloader_spec.rb
+++ spec/downloader_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe Downloader do
     let(:url) { 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml' }
     let(:macbeth_xml) { '<PLAY>...</PLAY>' }
 
+    subject { described_class.download_macbeth }
+
     it 'downloads the macbeth.xml' do
         allow(Net::HTTP).to receive(:get).with(URI(url)).and_return(macbeth_xml)
 
-        expect(described_class.download_macbeth).to eq(macbeth_xml)
+        is_expected.to eq(macbeth_xml)
     end
   end
 end
--- spec/xml_parser_spec.rb
+++ spec/xml_parser_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe XmlParser do
   describe '#parse_xml' do
     let(:xml) { 'any xml string' }
 
-      it 'parses string input to a nokogiri xml document' do
-        expect(described_class.parse_xml(xml).class).to eq(Nokogiri::XML::Document)
-      end
+    subject { described_class.parse_xml(xml).class }
+
+    it { is_expected.to eq(Nokogiri::XML::Document) }
   end
 end
